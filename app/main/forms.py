2bd60f2362cf40e5dab153b491efbe8cf955ecc1
josephndegwa <kariukindegwa98@gmail.com>
2022-02-08 14:53:17 +0300

    add email validation and routes

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,6 +1,5 @@
-import email
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 
@@ -9,3 +8,12 @@ class RegistrationForm(FlaskForm):
     email = StringField('Email',validators=[DataRequired(), Email()])
     password = PasswordField('Password',validators=[DataRequired()])
     confrim_password = PasswordField('Confirm Password',validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email',validators=[DataRequired(), Email()])
+    password = PasswordField('Password',validators=[DataRequired()])
+    remeber = BooleanField('Remember Me')
+    submit = SubmitField('Login')
--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,6 @@
 from flask import render_template,url_for
 from . import main 
+from .forms import RegistrationForm, LoginForm
 
 
 # Views
@@ -26,3 +27,27 @@ def about():
    
     
     return render_template('about.html')
+
+
+@main.route('/register')
+def regiater():
+
+    '''
+    View root page function that returns the register page and its data
+    '''
+    
+   
+    
+    return render_template('register.html')
+
+
+@main.route('/login')
+def login():
+
+    '''
+    View root page function that returns the login page and its data
+    '''
+    
+   
+    
+    return render_template('login.html')
