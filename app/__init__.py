89b001d8dc11ec1e5fc0bae957576223387aa94a
josephndegwa <kariukindegwa98@gmail.com>
2022-02-09 23:30:21 +0300

    update login manager

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,12 @@ from config import config_options
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 bcrypt = Bcrypt()
+login_manager = LoginManager()
 
 
 def create_app (config_name):
